de00e11c99570c0113cef43fe3c4cd9414910714
kevin-thankyou-lin <kevin.lin20000@gmail.com>
2018-11-03 16:07:19 -0700

    Add back server with hidden API key

---
+++ Server/APIkey.py
@@ -0,0 +1,13 @@
+
+version = "2017-09-21"
+iam_apikey = "QUY7_rDptJTu2yjx3QeM_AVXvwRAC757_pck77_vOQ7x"
+url = "https://gateway.watsonplatform.net/tone-analyzer/api"
+
+
+#"name": "Tone API",
+#"description": "IBM Watson Tone API",
+#"createdAt": "2018-11-03T05:13+0000",
+#"apiKey": "SE-3z--agPVMuGb-SNte44rCf3slE05gxCKgObaHqOQO"
+
+
+
---
+++ Server/server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import json
+from watson_developer_cloud import ToneAnalyzerV3
+import APIkey
+
+app = Flask(__name__)
+api = Api(app)
+
+tone_analyzer = ToneAnalyzerV3(
+    version= APIkey.version,
+    iam_apikey=APIkey.iam_apikey,
+    url=APIkey.url
+)
+
+def anger_score(data):
+    doc_tone = data.get('document_tone')
+    tones = doc_tone.get('tones')
+    threshold = 0
+
+    for tone in tones:
+        score = tone.get("score")
+        tone_id = tone.get("tone_id")
+        if score > threshold and tone_id == "anger":
+            return score
+
+class Tone(Resource):
+    def get(self, input):
+        text = input
+        tone_analysis = tone_analyzer.tone({'text': text},'application/json').get_result()
+        data = json.loads(json.dumps(tone_analysis, indent=2))
+        return is_angry(data), 200
+
+
+api.add_resource(Tone, "/tone/<string:input>")
+app.run(debug=True)
