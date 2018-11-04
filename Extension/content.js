03290c559441b0cf38129d0818be96b616167633
Jay Monga <jay.monga@gmail.com>
2018-11-04 00:59:49 -0700

    Add support for all input fields

--- Extension/content.js
+++ Extension/content.js
@@ -1,39 +1,34 @@
-var input_fields = document.querySelectorAll("input");
-var text_areas = document.querySelectorAll("textarea")
 var spaces = 0;
-for (var i = 0; i < input_fields.length; i++){
-    let box = input_fields[i];
-    let listener = box.addEventListener("input", function(event) {
+
+document.addEventListener("input", function(event) {
+    let t = event.target;
     let c = event.data;
-       if(c !== null && c===' '){
+    if (t.type == "text" || t.type == "input" || t.type == "textarea"){
+        if(c !== null && c===' '){
             spaces+=1;
-       }
-       if(spaces >= 2){
+        }
+        if(spaces >= 2){
            spaces = 0;
-           let text = box.value;
-           chrome.runtime.sendMessage({message: 'input', data: text}, function(response) {
-              if (response.value > .5){
-                    alert('Chill out');
-                }
-           });
-       }
-    });
-}
-for (var i = 0; i < text_areas.length; i++){
-    let box = text_areas[i];
-    let listener = box.addEventListener("input", function(event) {
-    let c = event.data;
-       if(c !== null && c===' '){
+           let text = t.value;
+            chrome.runtime.sendMessage({message: 'input', data: text}, function(response) {
+               if (response.value > .5){
+                     alert('Chill out');
+                 }
+            });
+        }
+    }
+    else if(t.type != "password"){
+        if(c !== null && c===' '){
             spaces+=1;
-       }
-       if(spaces >= 2){
+        }
+        if(spaces >= 2){
            spaces = 0;
-           let text = box.value;
-           chrome.runtime.sendMessage({message: 'input', data: text}, function(response) {
-              if (response.value > .5){
-                    alert('Chill out');
-                }
-           });
-       }
-    });
-}
+           let text = t.innerText;
+            chrome.runtime.sendMessage({message: 'input', data: text}, function(response) {
+               if (response.value > .5){
+                     alert('Chill out');
+                 }
+            });
+        }
+    }
+});
